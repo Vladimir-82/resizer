26f3673d65e5ef41fb01e3f6f5b065a6ce10bba0
vladimir <grun_gespenst@tut.by>
2022-08-23 16:48:50 +0300

    static added

--- app/forms.py
+++ app/forms.py
@@ -6,4 +6,7 @@ class AddPhoto(forms.ModelForm):
     class Meta:
         model = Photo
         fields = ['title', 'format', 'photo', 'width', 'height']
-        help_texts ='sss'
+        help_texts = {'format': "PNG/JPEG/GIF/RAW/TIFF/BMP/PSD",
+                      'width': "You can choose one of the options or two",
+                      'height': "You can choose one of the options or two"
+                      }
