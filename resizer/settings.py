162d74134a8fedc2cc923ff1d6807a883cdcbe8b
vladimir <grun_gespenst@tut.by>
2022-08-26 22:26:50 +0300

    media in app

--- resizer/settings.py
+++ resizer/settings.py
@@ -154,6 +154,5 @@ if "CI" in os.environ:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'app/', 'media')
 MEDIA_URL = '/media/'
-MEDIAFILES_DIRS = (BASE_DIR, 'media')
