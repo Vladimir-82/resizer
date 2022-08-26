db617c4ecc7b418bbfb0af91ec195e6126522201
vladimir <grun_gespenst@tut.by>
2022-08-26 20:50:35 +0300

    MEDIA_ROOT_2

--- resizer/settings.py
+++ resizer/settings.py
@@ -156,4 +156,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIAFILES_DIRS = (MEDIA_ROOT)
+MEDIAFILES_DIRS = (BASE_DIR, 'media/')
