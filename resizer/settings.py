068184d1981b10145c8a7197bd9b6a089688f5bf
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-09-27 13:11:18 +0300

    settings updating

--- resizer/settings.py
+++ resizer/settings.py
@@ -11,7 +11,7 @@ from .databases import get_database
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 IS_HEROKU = "DYNO" in os.environ
-HEROKU = False
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -79,15 +79,11 @@ MAX_CONN_AGE = 600
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if not HEROKU:
-    DATABASES = {
-        'default': get_database()
-    }
-else:
-    DATABASES = {}
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600,
-                                                  ssl_require=True
-                                                  )
+
+DATABASES = {
+    'default': get_database()
+}
+
 
 
 
